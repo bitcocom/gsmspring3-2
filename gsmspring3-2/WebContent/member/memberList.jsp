b551207a67cc710005f4a5f2ca7f10103850a165
GSM <GSM@DESKTOP-UUM6D3I>
2021-03-23 15:21:49 +0900

    gsm2

---
+++ gsmspring3-2/WebContent/member/memberList.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@page import="java.util.*" %>
+<%@page import="kr.gsm.model.*" %>    
+<%
+    List<MemberVO> list=(List<MemberVO>)request.getAttribute("list");
+
+%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ gsmspring3-2/src/kr/gsm/controller/MemberListController.java
@@ -0,0 +1,34 @@
+package kr.gsm.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.gsm.model.MemberDAO;
+import kr.gsm.model.MemberVO;
+
+@WebServlet("/memberList.do")
+public class MemberListController extends HttpServlet {	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		MemberDAO dao=new MemberDAO();
+		List<MemberVO> list=dao.memberList();
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out=response.getWriter();
+		out.println(list.toString());
+		//응답화면을 여기서 만들것인가? = Servlet
+		//View=>JSP(memberList.jsp)
+		//응답을 요청할 JSP파일을 얻어오기
+		request.setAttribute("list", list);
+		RequestDispatcher rd=request.getRequestDispatcher("member/memberList.jsp");
+		rd.forward(request, response); // memberList.jsp(실행)
+		
+	}
+}
